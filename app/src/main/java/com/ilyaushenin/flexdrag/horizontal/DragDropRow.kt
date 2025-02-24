0972dd3037731f1a4d4cac40c008194b4422de8a
Ilya Ushenin <ilyaushenin@gmail.com>
2025-02-24 10:51:39 +0300

    z-index

--- app/src/main/java/com/ilyaushenin/flexdrag/horizontal/DragDropRow.kt
+++ app/src/main/java/com/ilyaushenin/flexdrag/horizontal/DragDropRow.kt
@@ -15,10 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -26,7 +23,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.Job
+import androidx.compose.ui.zIndex
 import kotlinx.coroutines.launch
 
 @Composable
@@ -35,35 +32,11 @@ fun DragDropRow(
     onMove: (Int, Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val scope = rememberCoroutineScope()
-    var overScrollJob by remember { mutableStateOf<Job?>(null) }
     val dragDropListState = rememberDragDropRowState(onMove = onMove)
 
     LazyRow(
         modifier = modifier
-            .pointerInput(Unit) {
-                detectDragGesturesAfterLongPress(
-                    onDrag = { change, offset ->
-                        change.consume()
-                        dragDropListState.onDrag(offset = offset)
-
-                        if (overScrollJob?.isActive == true)
-                            return@detectDragGesturesAfterLongPress
-
-                        dragDropListState
-                            .checkForOverScroll()
-                            .takeIf { it != 0f }
-                            ?.let {
-                                overScrollJob = scope.launch {
-                                    dragDropListState.lazyListState.scrollBy(it)
-                                }
-                            } ?: kotlin.run { overScrollJob?.cancel() }
-                    },
-                    onDragStart = { offset -> dragDropListState.onDragStart(offset) },
-                    onDragEnd = { dragDropListState.onDragInterrupted() },
-                    onDragCancel = { dragDropListState.onDragInterrupted() }
-                )
-            }
+            .longPressChecker(dragDropListState = dragDropListState)
             .wrapContentSize()
             .padding(10.dp),
         state = dragDropListState.lazyListState
@@ -76,8 +49,10 @@ fun DragDropRow(
                     0f
                 }, label = "change box animation"
             )
+            val isDragged = index == dragDropListState.currentIndexOfDraggedItem
             Column(
                 modifier = Modifier
+                    .zIndex(if (isDragged) 1f else 0f)
                     .graphicsLayer {
                         translationX = animatedTranslationX
                     }
@@ -93,4 +68,30 @@ fun DragDropRow(
             Spacer(modifier = Modifier.width(10.dp))
         }
     }
+}
+
+@Composable
+fun Modifier.longPressChecker(
+    dragDropListState: DragDropRowState,
+): Modifier {
+    val scope = rememberCoroutineScope()
+    return this.then(
+        pointerInput(Unit) {
+            detectDragGesturesAfterLongPress(
+                onDrag = { change, offset ->
+                    change.consume()
+                    dragDropListState.onDrag(offset = offset)
+
+                    val overScroll = dragDropListState.checkForOverScroll()
+                    if (overScroll != 0f) {
+                        scope.launch {
+                            dragDropListState.lazyListState.scrollBy(overScroll)
+                        }
+                    }
+                },
+                onDragStart = { offset -> dragDropListState.onDragStart(offset) },
+                onDragEnd = { dragDropListState.onDragInterrupted() },
+                onDragCancel = { dragDropListState.onDragInterrupted() }
+            )
+        })
 }
--- app/src/main/java/com/ilyaushenin/flexdrag/ui/MainActivity.kt
+++ app/src/main/java/com/ilyaushenin/flexdrag/ui/MainActivity.kt
@@ -8,20 +8,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.toMutableStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ilyaushenin.flexdrag.data.move
 import com.ilyaushenin.flexdrag.horizontal.DragDropRow
 import com.ilyaushenin.flexdrag.ui.theme.FlexDragComposeTheme
-import com.ilyaushenin.flexdrag.vertical.DragDropList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
